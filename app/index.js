aa12dff05f9a0ce11a1965f7e6a97a055bedb9c1
Eric Mann <eric@eamann.com>
2014-10-29 16:28:31 -0700

    Add in some templates

--- app/index.js
+++ app/index.js
@@ -1,32 +1,91 @@
 'use strict';
 
-var yeoman = require('yeoman-generator'),
-	chalk = require('chalk'),
-	path = require('path');
+var generator = require( 'yeoman-generator' ),
+	async = require( 'async' ),
+	chalk = require( 'chalk' ),
+	path = require( 'path' );
 
-var LibGenerator = yeoman.generators.Base.extend({
-	constructor: function() {
-		yeoman.generators.Base.apply( this, arguments );
-		var dirPath = '../templates';
-		this.sourceRoot( path.join( __dirname, dirPath ) );
-	},
+var LibGenerator = generator.Base.extend(
+	{
+		constructor: function () {
+			var dirPath = '../templates';
 
-	init: function() {
-		this.pkg = require('../package.json');
+			generator.Base.apply( this, arguments );
 
-		this.on( 'end', function() {
+			this.sourceRoot( path.join( __dirname, dirPath ) );
+		},
 
-		} );
-	},
+		options: function () {
+			var done = this.async();
+			this.basename = path.basename( this.env.cwd );
 
-	welcome: function() {
-		this.log( chalk.magenta( 'Thanks for generating a WordPress library with yo!' ) );
+			var prompts = [
+				{
+					name:    'namespace',
+					message: 'Project Namespace',
+					default: 'TenUp'
+				},
+				{
+					name:    'projectName',
+					message: 'Project Name',
+					default: 'WPLib'
+				},
+				{
+					name:    'description',
+					message: 'Description',
+					default: 'The best WordPress extension ever made!'
+				},
+				{
+					name:    'projectHome',
+					message: 'Project homepage'
+				},
+				{
+					name:    'authorName',
+					message: 'Author name',
+					default: this.user.git.name
+				},
+				{
+					name:    'authorEmail',
+					message: 'Author email',
+					default: this.user.git.email
+				},
+				{
+					name:    'authorUrl',
+					message: 'Author URL'
+				}
+			];
+
+			// Gather initial settings
+			this.prompt( prompts, function ( properties ) {
+				this.opts = properties;
+
+				this.className = this.opts.projectName.replace( /[\s]/g, '_' ).replace( /[-]/g, '_' );
+
+				done();
+			}.bind( this ) );
+		},
+
+		init: function () {
+			this.pkg = require( '../package.json' );
+
+			this.on( 'end', function () {
+
+			} );
+		},
+
+		welcome: function () {
+			this.log( chalk.magenta( 'Thanks for generating a WordPress library with yo!' ) );
+		},
+
+		composer: function () {
+			this.template( '_composer.json', 'composer.json' );
+		},
+
+		git: function () {
+			this.copy( '_gitignore', '.gitignore' );
+		}
 	}
-
-	// Prompts
-
-	// Output
-});
+);
 
 // Export the module
-module.exports = Generator;
+module.exports = LibGenerator;
